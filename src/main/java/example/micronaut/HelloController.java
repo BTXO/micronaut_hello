f6fd429065a96a75519f5be4b419425d82bb175e
EC2 Default User <ec2-user@ip-172-31-43-123.ap-northeast-1.compute.internal>
2020-05-10 12:24:12 +0000

    initial commit

---
+++ src/main/java/example/micronaut/HelloController.java
@@ -0,0 +1,16 @@
+// path/your/prject/complete/src/main/java/example/micronaut/HelloController.java
+package example.micronaut;
+
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Produces;
+
+@Controller("/hello")
+public class HelloController {
+    @Get("/")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String index() {
+        return "Hello World";
+    }
+}
